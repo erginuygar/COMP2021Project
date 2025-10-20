b0377946626180c3ad27bd096cc6d3b03c2310cb
erginuygar <steamuygarergin06@gmail.com>
2025-10-20 20:07:09 +0800

    Update Logger.java

---
+++ SampleProject/src/hk/edu/polyu/comp/comp2021/clevis/model/Logger.java
@@ -0,0 +1,29 @@
+package hk.edu.polyu.comp.comp2021.clevis.model;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Logger {
+    private final static String file_name = "log.txt";
+    private static PrintWriter writer;
+    private static int index = 0;
+
+    static{
+
+        try{
+            writer = new PrintWriter(new FileWriter(file_name, true));
+
+        }
+        catch (IOException e){
+            System.err.println("Failed to initialize logger: " + e.getMessage());
+        }
+    }
+
+    private static void Log(String method_name) {
+        if(writer != null){
+            String 
+        }
+    }
+}
